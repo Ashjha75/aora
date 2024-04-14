5b8dd5cf0c25c1955938bb8c8d7fc09dbecb60f6
Ashish Jha <ajha5645@gmail.com>
2024-04-14 13:02:22 +0530

    aora first commit

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-[#1f4662]">
+            <Text className="text-3xl text-yellow-500 font-pblack">Aora!</Text>
+            <StatusBar style="auto" />
+            <Link href="/profile" style={{ color: 'blue' }}>
+                <Text>Go To Profile</Text>
+            </Link>
+
+        </View >
+    );
+}
+
